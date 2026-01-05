658408c33ba469e540998f270d6ef7cbc3680e2f
Alex Buchanan <alex.buchanan67@hotmail.com>
2026-01-04 21:50:00 -0500

    init storage

---
+++ packages/ts-logkit/src/types/storage.ts
@@ -0,0 +1,21 @@
+import { Config } from "./config";
+
+/** System-wide logging configuration is just an array of Config */
+export type SystemConfig = Config[];
+
+/**
+ * Interface for storage of logger configs
+ */
+export interface ILoggingStorage {
+  /** Get the full system logging configuration (all loggers) */
+  list(): Promise<SystemConfig>;
+
+  /** Replace the full system logging configuration */
+  setAll(configs: SystemConfig): Promise<void>;
+
+  /** Get a single logger's config by name */
+  get(name: string): Promise<Config>;
+
+  /** Save or update a single logger config */
+  set(config: Config): Promise<void>;
+}
